c27f3b77639d460c96b4ec3899386c1f8b2f9679
Suganya Ponnusamy <suganya.sugu88@gmail.com>
2025-03-18 22:01:30 -0500

    Debugging, Implicit& Explicit Type Conversions Done

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,37 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
-console.log("The result is: " + result);
+let result = Number("5") - 2;//explicitly converting string to number type
+console.log("The result is: " + result);//displaying the result 
 
-let isValid = Boolean("false");
-if (isValid) {
-    console.log("This is valid!");
+let isValid = Boolean("false");// Explicitly converting the string "false" to a boolean also "false" is a non-empty string, and in JavaScript, any non-empty string is truthy
+
+if (isValid) {//Checking if the boolean value is true
+    console.log("This is valid!");//This will always print because "false" as a string is truthy
 }
 
-let age = "25";
+let age = Number("25");//explicitly converting string to number 
 let totalAge = age + 5;
-console.log("Total Age: " + totalAge);
+console.log("Total Age: " + totalAge);// displaying age
+
+//Example of Implicit Type Conversion
+console.log("Example of Implicit Type Conversion");
+let babyAge= "Just Born";//declaring a boolean implicitly
+if (babyAge){//here the boolean value is considered truthy
+  console.log(babyAge + " Yay! Congratulations on the Baby!");//display baby age
+}
+
+babyAge = "0"+1;//implicit string concatenation happening
+console.log(`You are ${babyAge} now, Happy 1st Birthday Baby`);//displaying baby age
+
+//Example of Explicit Type Conversion
+// Convert null to a boolean
+console.log("Example of Explicit Type Conversion");
+let nullValue = null;// declaring a variable nullValue to null
+console.log("Before:", nullValue, "| Type:", typeof nullValue);// displaying the variable and using type of to show the variable data type
+console.log("After:", Boolean(nullValue), "| Type:", typeof Boolean(nullValue));// explicitly declaring data type for the nullValue and using type of to show how it displays
+
+// Convert undefined to a boolean
+let undefinedValue;//declaring a undefined variable without initializing it
+console.log("Before:", undefinedValue, "| Type:", typeof undefinedValue);//displaying the undefinedValue as it is using typeof
+console.log("After:", Boolean(undefinedValue), "| Type:", typeof Boolean(undefinedValue));//explicitely changing it to Be=oolean and displaying undefinedValue
